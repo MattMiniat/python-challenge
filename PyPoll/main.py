bc40e3f3d29f8b853c377c53120425de2391f643
Matthew Miniat <60368690+Minitaurr@users.noreply.github.com>
2020-02-22 10:34:46 -0600

    Python project files
    First push to repo. Python Bank files.

---
+++ PyPoll/main.py
@@ -0,0 +1,12 @@
+# create csvreader and for loop to read through the poll data
+# Create list and loop and return all unique candidates who recieved votes (votes = true)
+# Create for loop adding up all of the votes
+    #Create separate loops to add up how many votes each candidate recieved
+    #divide and format the answers
+#Create a dictionary using candidates as keys and their votes as values, returning the max value as the winner
+#  
+#
+#
+#
+#
+#
