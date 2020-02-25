dc98c6c7fb64dcb2fd622a4677ed2d2cb267456d
Matthew Miniat <60368690+Minitaurr@users.noreply.github.com>
2020-02-25 01:04:12 -0600

    Submissions for python project

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,9 +4,54 @@ import csv
 # path to csv file
 csvpath = os.path.join("Resources","election_data.csv")
 
-def voterCount(list)
-vote_count + 1
-    print(str(vote_count))
+total_votes = []
+assignedVotes = []
+Candidates = []
+electionDict = {}
+
+def Results():
+    print("Election Results")
+    print("----------------------------------------")
+    getTotalVotes()
+    print("----------------------------------------")
+    getCandidates()
+    print("----------------------------------------")
+    # getWinner()
+
+
+def getVotes(election_data):
+    total_votes.append(election_data[2])
+
+
+def getTotalVotes():
+    total = len(total_votes)
+    print("Total Votes: " + str(total))
+
+
+
+def getCandidates():
+    
+    candidatesSet = set(total_votes)
+    NewList = list(candidatesSet)
+    Candidates = NewList
+    electionDict = {i : 0 for i in Candidates}
+    for key in electionDict:
+        for j in range(len(total_votes)):
+            if key == total_votes[j]:
+                electionDict[key] += 1
+        print(f" " + key, electionDict[key], votePercent(electionDict[key], len(total_votes)))
+    
+
+    print("----------------------------------------")
+    Winner = max(electionDict, key=electionDict.get)
+    print("Winner!: "+ Winner)
+        
+
+
+def votePercent(votesAssigned, totalVotes):
+    PercentOfVote = str(round((votesAssigned / totalVotes * 100)))
+    output = str("(" + PercentOfVote + "%)")
+    return output
 
 
 with open(csvpath) as csvfile:
@@ -14,36 +59,21 @@ with open(csvpath) as csvfile:
     print("One moment please...")
     print(" ")
 
+    
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
     # Read the header row first so that it's not counted with the following data in the for loops.
     csv_header = next(csvreader)
-
-    VoterID = []
-    Counties = []
-    Candidates = []
-    
-    for row in csvreader
-        
     
 
 
+    for row in csvreader:
+        getVotes(row)
+    Results()
 
 
 
 
-# 
-# 
-# Create csvreader and for loop to read through the poll data
-# Create list and loop and return all unique candidates who recieved votes (votes = true)
-# Create for loop adding up all of the votes
-    #Create separate loops to add up how many votes each candidate recieved
-    #divide and format the answers
-#Create a dictionary using candidates as keys and their votes as values, returning the max value as the winner
-#  
-#
-#
-#
-#
-#
+
+
