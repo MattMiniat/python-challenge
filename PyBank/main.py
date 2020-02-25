be69b5cf539db7f51d57bb1b48347db07fb7265d
Matthew Miniat <60368690+Minitaurr@users.noreply.github.com>
2020-02-25 13:33:13 -0600

    Submission after deadline w/ comments
    The code is unchanged. I only added comments.

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,6 +8,7 @@ Dates = []
 Totals = []
 Changes = []
 
+# main function used to print out the results
 def Financial_Analysis():
     print(" ")
     print("Financial Analysis")
@@ -19,60 +20,60 @@ def Financial_Analysis():
     get_I_date()
     get_D_date()
 
-
+# functions that calls other functions to populate lists
 def FinancialLists(budget_data):
     getDates(budget_data)
     getTotals(budget_data)
 
-
+#function used to populate 'Dates' list
 def getDates(budget_data):
     date = str(budget_data[0])
     Dates.append(date)
 
-
+#function used to populate 'Totals' list
 def getTotals(budget_data):
     monthly_amount = int(budget_data[1])
     Totals.append(monthly_amount)
 
-
+#function used to populate 'Changes' list
 def getChanges():
     for i in range(1, len(Totals)):
         revenue_change = Totals[i] - Totals[i - 1]
         Changes.append(revenue_change)
 
-
+#function used to print out the total amounnt of months by counting the number of items in 'Dates'
 def Total_Months():
     Months_Num = (len(Dates))
     print(f"Total Months :" + str(Months_Num))
 
-
+#function used to print out the net total of the csv
 def netTotal():
     netTotal = sum(Totals)
     print(f"Net Total: $" + str(netTotal))
 
-
+#function used to find the avergage change in the 'Changes' list
 def Avg_Change():
     avg_change = sum(Changes)/len(Changes)
     print(f"Average Change: $" + str(round(avg_change, 2)))
 
-
+#function used to find the max in Changes
 def greatestIncrease():
     greatest_Profit = max(Changes)
     return greatest_Profit
 
-
+#function used to find the min in Changes
 def greatestDecrease():
     greatest_Loss = min(Changes)
     return greatest_Loss
 
-
+#function used to find the date of the max in Changes and print it.
 def get_I_date():
     for i in range(len(Totals)):
         if Totals[i] - Totals[i - 1] == greatestIncrease():
             greatest_increase_date = Dates[i]
     print(f"Greatest increase in revenue: " + greatest_increase_date + " ($" + str(greatestIncrease()) + ")" )
 
-
+#function used to find the date of the min in Changes and print it
 def get_D_date():
     for i in range(len(Totals)):
         if Totals[i] - Totals[i - 1] == greatestDecrease():
@@ -81,15 +82,17 @@ def get_D_date():
 
 
 
-
+# Read in the CSV file
 with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    # Read the header row first so that it's not counted with the following data in the for loops.
+    # Read the header row first so that it's not counted with the following data in the for loop.
     csv_header = next(csvreader)
     
+    # Loop through the data and populate the lists first
     for row in csvreader:
         FinancialLists(row)
+    #print the results of the analysis
     Financial_Analysis()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,11 +4,13 @@ import csv
 # path to csv file
 csvpath = os.path.join("Resources","election_data.csv")
 
+# lists to be called by functions
 total_votes = []
 assignedVotes = []
 Candidates = []
 electionDict = {}
 
+# Main function used to organize and print results 
 def Results():
     print("Election Results")
     print("----------------------------------------")
@@ -18,36 +20,55 @@ def Results():
     print("----------------------------------------")
     # getWinner()
 
-
+# Function used to populate the 'total_votes' list 
 def getVotes(election_data):
     total_votes.append(election_data[2])
 
-
+# After 'getVotes()' is used in the with loop, 
+# This function counts the number of votes in the list and prints out the total number of votes
 def getTotalVotes():
     total = len(total_votes)
     print("Total Votes: " + str(total))
 
 
+# Unfortunately, I could not figure out how to split this next function into separate functions as they rely on each other,
+# and for some reason would only return empty. 
+# Each part of the function is as follows:
+# 1) Filters out the names of each unique candidate in 'total_votes' and add them to the 'Candidates' list, 
+
+# 2) Convert the list into a dictionary: electionDict
+
+# 3) Iterates through 'total_votes', comparing the keys in electionDict to the items in total_votes
+#       for each time an item equals the dictionary's key, the value of that key is incremented by 1
+
+# 4) Prints out the keys of electionDict, calls votePercent() for each entry in electionDict, and prints out the value of each key.
+
+# 5) uses max() to detemine the maximum value in electionDict and then prints the key as the winner to the terminal.
 
 def getCandidates():
-    
+# 1)
     candidatesSet = set(total_votes)
     NewList = list(candidatesSet)
     Candidates = NewList
+
+# 2)
     electionDict = {i : 0 for i in Candidates}
+
+# 3)
     for key in electionDict:
         for j in range(len(total_votes)):
             if key == total_votes[j]:
                 electionDict[key] += 1
+# 4)
         print(f" " + key, electionDict[key], votePercent(electionDict[key], len(total_votes)))
     
-
+# 5)
     print("----------------------------------------")
     Winner = max(electionDict, key=electionDict.get)
     print("Winner!: "+ Winner)
         
 
-
+# function used to find the percent of the total_votes and print it out as a string.
 def votePercent(votesAssigned, totalVotes):
     PercentOfVote = str(round((votesAssigned / totalVotes * 100)))
     output = str("(" + PercentOfVote + "%)")
@@ -63,13 +84,14 @@ with open(csvpath) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    # Read the header row first so that it's not counted with the following data in the for loops.
+    # Read the header row first so that it's not counted with the following data in the for loop.
     csv_header = next(csvreader)
     
 
-
+    #Loop through the data and find the total number of votes in the csv
     for row in csvreader:
         getVotes(row)
+    #print out the results of the election and determine the winner.
     Results()
 
 
